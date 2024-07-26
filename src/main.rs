5fbf24ee7ee379a7d2c20b57efe528b7d0364d1e
Thibaut de Saivre <thibaut2saivre@gmail.com>
2024-07-26 16:36:43 +0200

    Do hex validation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use notify::event::{AccessKind, AccessMode};
 use notify::RecursiveMode::NonRecursive;
 use notify::{recommended_watcher, EventKind, Watcher};
+use regex::Regex;
 use serde_derive::Deserialize;
 
 /// Simple native program to watch and send base16 color schemes to browsers
@@ -62,9 +63,44 @@ fn main() -> Result<()> {
   Ok(())
 }
 
-/// Read colors from a TOML file.
+/// Read colors from a TOML file and validate them.
 fn read_colors(path: &str) -> Result<Colors> {
   let content =
     std::fs::read_to_string(path).context(format!("Failed to read colors TOML file: {}", &path))?;
-  toml::from_str(&content).context("Failed to parse colors TOML file")
+  let colors = toml::from_str(&content).context("Failed to parse colors TOML file")?;
+  validate_hex_colors(&colors)?;
+
+  Ok(colors)
+}
+
+/// Check that all colors are valid hex colors
+fn validate_hex_colors(colors: &Colors) -> Result<()> {
+  let re = Regex::new(r"^#(?:[0-9a-fA-F]{3}){1,2}$|^#(?:[0-9a-fA-F]{4}){1,2}$").unwrap();
+
+  let hex_colors = [
+    ("base00", &colors.base00),
+    ("base01", &colors.base01),
+    ("base02", &colors.base02),
+    ("base03", &colors.base03),
+    ("base04", &colors.base04),
+    ("base05", &colors.base05),
+    ("base06", &colors.base06),
+    ("base07", &colors.base07),
+    ("base08", &colors.base08),
+    ("base09", &colors.base09),
+    ("base0A", &colors.base0A),
+    ("base0B", &colors.base0B),
+    ("base0C", &colors.base0C),
+    ("base0D", &colors.base0D),
+    ("base0E", &colors.base0E),
+    ("base0F", &colors.base0F),
+  ];
+
+  for (label, hex) in hex_colors {
+    if !re.is_match(hex) {
+      return Err(anyhow!("Invalid hex color for {}: {}", label, hex));
+    }
+  }
+
+  Ok(())
 }
